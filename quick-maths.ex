7e43937074e6888a3a9618b59015744d4a3a7537
Sean <sean-p-96@hotmail.de>
2020-12-29 08:55:27 +0100

    Add is_prime

--- quick-maths.ex
+++ quick-maths.ex
@@ -4,6 +4,22 @@ defmodule QuickMaths do
   @spec circle_area(number) :: number
   def circle_area(r), do: r * r * @pi
 
+  @spec is_prime(number) :: boolean
+  def is_prime(1), do: false
+  def is_prime(2), do: true
+  def is_prime(n) when rem(n, 2) == 0, do: false
+  def is_prime(n), do: is_prime(n, 3)
+  def is_prime(n, m) do
+    case m > :math.sqrt(n) do
+      false ->
+        case rem(n, m) == 0 do
+                true -> false
+                false -> is_prime(n, m+1)
+        end
+      true -> true
+    end
+  end
+
   @spec fibo(number) :: number
   def fibo(n) do
     case n do
